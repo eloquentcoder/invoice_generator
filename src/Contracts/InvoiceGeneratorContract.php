e77f6ecd903723907394db42fa5b05d6c466bfd6
Mfonn <eloquentintech@gmail.com>
2024-01-04 23:18:28 +0100

    Greated an contract for the invoice generator class

---
+++ src/Contracts/InvoiceGeneratorContract.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+
+namespace Eloquentcoder\InvoiceGenerator\Contracts;
+
+use DateTimeInterface;
+
+interface InvoiceGeneratorContract
+{
+    public function generate(
+        string $value,
+        DateTimeInterface $date,
+        string|int $identifier
+    ): string;
+}
